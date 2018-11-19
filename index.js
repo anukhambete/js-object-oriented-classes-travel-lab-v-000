788c2d409bb3c4ed35384f69c5701c76b2a0cf30
Anu Khambete <anukhambete@gmail.com>
2018-11-19 18:16:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ class Route {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
+  blocksTravelled (){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let vertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    let horizontal = Math.abs(eastWest.indexOf(route.beginningLocation.horizontal) - eastWest.indexOf(route.endingLocation.horizontal));
+    return vertical + horizontal;
+  }
 
-  
 }
